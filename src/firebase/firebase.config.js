28feee4e3bcc7bc911180c133fe881cf8b58fcd5
BakiAbdullah <shatil605@gmail.com>
2023-04-18 10:01:30 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDdcOWR4uK5phell4ItiHZIETcqgg99E7U",
+  authDomain: "the-news-dragon-8d296.firebaseapp.com",
+  projectId: "the-news-dragon-8d296",
+  storageBucket: "the-news-dragon-8d296.appspot.com",
+  messagingSenderId: "457213627265",
+  appId: "1:457213627265:web:bf9f020cee559b74c27680"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
